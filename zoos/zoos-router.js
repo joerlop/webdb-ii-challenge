5c458559261317df86446b8ad1d74d18b4272d4d
joerlop <jj.erlich155@gmail.com>
2019-06-04 14:49:25 -0500

    Get and post methods developed

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -14,9 +14,20 @@ const knexConfig = {
 const db = knex(knexConfig);
 
 router.post('/', (req, res) => {
-  db("zoos").insert(req.body, "id").then(ids => {
+  db("zoos").insert(req.body, "id")
+  .then(ids => {
     res.status(201).json(ids)
   }).catch(err => {
+    res.status(500).json(err)
+  })
+});
+
+router.get('/', (req, res) => {
+  db("zoos")
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(err => {
     res.status(500).json(err)
   })
 });
